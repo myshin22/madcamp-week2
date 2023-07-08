82937fcc7245c31629e4a225dc362850f1e467f4
minyoung <ericashin1@g.skku.edu>
2023-07-08 15:05:55 +0900

    third try

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -172,9 +177,93 @@ class AddScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
+      appBar: AppBar(
+        title: Text('내 맛집 리스트'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              openGallery(context);
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Text('Add Screen'),
+      ),
+    );
+  }
+
+  void openGallery(BuildContext context) async {
+    final picker = ImagePicker();
+    final List<XFile> selectedImages = [];
+
+    for (int i = 0; i < 5; i++) {
+      XFile? image = await picker.pickImage(
+        source: ImageSource.gallery,
+        maxWidth: 800,
+        maxHeight: 800,
+        imageQuality: 80,
+      );
+
+      if (image != null) {
+        selectedImages.add(image);
+      } else {
+        break;
+      }
+    }
+
+    if (selectedImages.isNotEmpty) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => GetPictureScreen(selectedImages),
+        ),
+      );
+    }
+  }
+}
+
+class GetPictureScreen extends StatelessWidget {
+  final List<XFile> selectedImages;
+
+  GetPictureScreen(this.selectedImages);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('글 쓰기'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.upload_sharp),
+            onPressed: () {
+              //upload 버튼이 눌리면?
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: selectedImages.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Image.file(
+                File(selectedImages[index].path),
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 8),
+              TextField(
+                decoration: InputDecoration(
+                hintText: '문구를 입력하세요',
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          );
+        },
       ),
     );
   }
