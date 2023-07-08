d941a2547f0d1db5cae2e832a4410943d0dcc09b
minyoung <ericashin1@g.skku.edu>
2023-07-08 21:57:56 +0900

    설정 부분 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:multi_image_picker/multi_image_picker.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -95,75 +93,64 @@ class SearchScreen extends StatelessWidget {
 
 class FeedItem{
   final String username;
+  final String profileImageUrl;
   final String imageUrl;
   final String caption;
 
-  FeedItem({required this.username,required this.imageUrl, required this.caption});
+  FeedItem(
+      {required this.username,
+      required this.profileImageUrl,
+      required this.imageUrl,
+      required this.caption});
 }
 
 
-class FeedScreen extends StatefulWidget {
-  @override
-  _FeedScreenState createState() => _FeedScreenState();
-}
-
-class _FeedScreenState extends State<FeedScreen>{
-  List<FeedItem> feedItems = []; //서버에서 가져온 데이터 저장할 리스트
-
-  //데이터를 가져오는 비동기 함수
-  Future<void>fetchData()async{
-    //데이터를 서버에서 가져오는 비동기 작업 수행
-    //가져온 데이터를 FeedItem으로 변환하여 feedItems 리스트에 저장
-    //setState() 호출하여 UI 갱신
-  }
-
-  @override
-  void initState(){
-    super.initState();
-    fetchData(); // iniState에서 데이터를 가져오는 비동기 함수 호출
-  }
+class FeedScreen extends StatelessWidget {
+  final List<FeedItem> feedItems = [
+    FeedItem(
+      username: 'user1',
+      profileImageUrl: 'assets/신민영_증명사진2023.jpg',
+      imageUrl: 'assets/맛집1.jpg',
+      caption: '정말정말맛있다.',
+    ),
+    // Add more feed items as needed
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText:'어느 지역 맛집을 알고 싶으세요?',
-                prefixIcon:Icon(Icons.search),
+      body: ListView.builder(
+        itemCount: feedItems.length,
+        itemBuilder: (context, index) {
+          final feedItem = feedItems[index];
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(feedItem.profileImageUrl),
+                ),
+                title: Text(feedItem.username),
               ),
-              onChanged: (value){
-                //검색어 입력하며 어떻게 됨?
-              },
-              onSubmitted: (value){
-                //검색어 제출하면 어떻게 됨?
-              },
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: feedItems.length,
-              itemBuilder: (context, index) {
-                final feedItem = feedItems[index];
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(feedItem.imageUrl),
-                  ),
-                  title: Text(feedItem.username),
-                  subtitle: Text(feedItem.caption),
-                );
-              },
-            ),
-          ),
-        ],
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  feedItem.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          // 액티비티(또는 다른 화면)을 표시하는 로직을 추가하세요
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SearchScreen()),
+          );
         },
         label: Text('지도'),
         icon: Icon(Icons.map),
@@ -174,7 +161,20 @@ class _FeedScreenState extends State<FeedScreen>{
   }
 }
 
-class AddScreen extends StatelessWidget {
+class AddScreen extends StatefulWidget {
+  @override
+  _AddScreenState createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  List<String> placeList = [
+    '장소1 - 주소1',
+    '장소2 - 주소2',
+    '장소3 - 주소3',
+    '장소4 - 주소4',
+    '장소5 - 주소5'
+  ]; //여기 실제로는 주소 데이터로 받아오자
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -184,94 +184,139 @@ class AddScreen extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              openGallery(context);
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text('Add Screen'),
-      ),
-    );
-  }
-
-  void openGallery(BuildContext context) async {
-    List<Asset> selectedImages = [];
-
-    try {
-      selectedImages = await MultiImagePicker.pickImages(
-        maxImages: 5,
-        enableCamera: true,
-      );
-    } catch (e) {
-      //예외?
-    }
-
-    if (selectedImages.isNotEmpty) {
-      List<XFile> xFiles = await Future.wait(
-        selectedImages.map((asset) => assetToFile(asset)),
-      );
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => GetPictureScreen(xFiles),
-        ),
-      );
-    }
-  }
-}
-
-Future<XFile> assetToFile(Asset asset) async {
-  final byteData = await asset.getByteData();
-  final tempDirectory = await getTemporaryDirectory();
-  final tempPath = '${tempDirectory.path}/${asset.name}';
-  final tempFile = File(tempPath);
-  await tempFile.writeAsBytes(byteData.buffer.asUint8List());
-
-  return XFile(tempPath);
-}
-
-class GetPictureScreen extends StatelessWidget {
-  final List<XFile> selectedImages;
-
-  GetPictureScreen(this.selectedImages);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('글 쓰기'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.upload_sharp),
-            onPressed: () {
-              //upload 버튼이 눌리면?
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  String name = '';
+                  String address = '';
+                  return AlertDialog(
+                    title: Text('장소 추가'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          onChanged: (value) {
+                            name = value;
+                          },
+                          decoration: InputDecoration(hintText: '장소 이름'),
+                        ),
+                        TextField(
+                          onChanged: (value) {
+                            address = value;
+                          },
+                          decoration: InputDecoration(hintText: '주소'),
+                        )
+                      ],
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('저장'),
+                        onPressed: () {
+                          setState(() {
+                            placeList.add('$name - $address');
+                          });
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
             },
           ),
         ],
       ),
       body: ListView.builder(
-        itemCount: selectedImages.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Image.memory(
-                File(selectedImages[index].path).readAsBytesSync(),
-                height: 200,
-                width: 200,
-                fit: BoxFit.cover,
+          itemCount: placeList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(placeList[index]),
+              onTap: () {
+                // 여기에 특정 장소를 눌렀을 때의 로직을 추가하세요
+                print('${placeList[index]}가 선택되었습니다.');
+              },
+            );
+          }),
+    );
+  }
+
+  Future<void> openGallery(BuildContext context) async {
+    try {
+      List<XFile>? resultList = await ImagePicker().pickMultiImage(
+        maxWidth: 1920,
+        maxHeight: 1080,
+        imageQuality: 90,
+      );
+
+      if (resultList != null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => GetPictureScreen(resultList),
+          ),
+        );
+      }
+    } catch (e) {
+      // 예외 처리
+    }
+  }
+}
+
+class GetPictureScreen extends StatefulWidget {
+  final List<XFile> images;
+
+  GetPictureScreen(this.images);
+
+  @override
+  _GetPictureScreenState createState() => _GetPictureScreenState();
+}
+
+class _GetPictureScreenState extends State<GetPictureScreen> {
+  String caption = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('글쓰기'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.upload_sharp),
+            onPressed: () {
+              //upload 버튼을 누르면 서버 상에 보낼 수 있도록 코드를 짜야 돼
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              itemCount: widget.images.length,
+              itemBuilder: (context, index) {
+                return Image.file(
+                  File(widget.images[index].path),
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Add caption',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '이 식당은 어떤 점이 좋았나요?',
-                ),
-              ),
-              SizedBox(height: 16),
-            ],
-          );
-        },
+              onChanged: (value) {
+                setState(() {
+                  caption = value;
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -281,9 +326,61 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
+      appBar: AppBar(
+        title: Text('Profile Screen'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsScreen()),
+              );
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Text('Profile Screen'),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('설정'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('연결관리'),
+          ),
+          ListTile(
+            title: Text('네이버 지도 연결'),
+            onTap: () {
+              // 항목 1이 선택되었을 때의 코드
+            },
+          ),
+          ListTile(
+            title: Text('친구추가'),
+          ),
+          ListTile(
+            title: Text('연락처로 추가하기'),
+            onTap: () {
+              // 친구 1이 선택되었을 때의 코드
+            },
+          ),
+          ListTile(
+            title: Text('초대장 보내기'),
+            onTap: () {
+              // 친구 2가 선택되었을 때의 코드
+            },
+          ),
+        ],
       ),
     );
   }
