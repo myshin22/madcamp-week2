3b752edc8c510d8151b38727769268d83289f5f8
minyoung <ericashin1@g.skku.edu>
2023-07-08 12:22:42 +0900

    second try

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class _MyAppState extends State<MyApp> {
         appBar: null,
         body: _screens[_selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black,
           currentIndex: _selectedIndex,
           onTap: _onItemTapped,
           items: [
@@ -87,32 +88,72 @@ class SearchScreen extends StatelessWidget {
   }
 }
 
+class FeedItem{
+  final String username;
+  final String imageUrl;
+  final String caption;
 
-class FeedScreen extends StatelessWidget {
+  FeedItem({required this.username,required this.imageUrl, required this.caption});
+}
+
+
+class FeedScreen extends StatefulWidget {
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen>{
+  List<FeedItem> feedItems = []; //서버에서 가져온 데이터 저장할 리스트
+
+  //데이터를 가져오는 비동기 함수
+  Future<void>fetchData()async{
+    //데이터를 서버에서 가져오는 비동기 작업 수행
+    //가져온 데이터를 FeedItem으로 변환하여 feedItems 리스트에 저장
+    //setState() 호출하여 UI 갱신
+  }
+  @override
+  void initState(){
+    super.initState();
+    fetchData(); // iniState에서 데이터를 가져오는 비동기 함수 호출
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText:'어느 지역 맛집을 알고 싶으세요?',
-                prefixIcon: Icon(Icons.search),
+      body: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText:'어느 지역 맛집을 알고 싶으세요?',
+                  prefixIcon:Icon(Icons.search),
+                ),
+                onChanged: (value){
+                  //검색어 입력하며 어떻게 됨?
+                },
+                onSubmitted: (value){
+                  //검색어 제출하면 어떻게 됨?
+                },
               ),
-              onChanged: (value) {
-                // 입력된 검색어 처리
-              },
-              onSubmitted: (value) {
-                // 엔터 키를 눌러 검색어 제출
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: feedItems.length,
+              itemBuilder: (context, index) {
+                final feedItem = feedItems[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(feedItem.imageUrl),
+                  ),
+                  title: Text(feedItem.username),
+                  subtitle: Text(feedItem.caption),
+                );
               },
             ),
-            SizedBox(height: 16.0),
-            Text('Search Screen'),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
