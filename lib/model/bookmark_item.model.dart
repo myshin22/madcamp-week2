c04aad97340092d7d3ef76db300a371252233dc5
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-11 11:17:38 +0900

    feat: add bookmark_item model

---
+++ lib/model/bookmark_item.model.dart
@@ -0,0 +1,13 @@
+class BookmarkItem {
+  final String name;
+  final String address;
+  final num px;
+  final num py;
+
+  BookmarkItem({
+    required this.name,
+    required this.address,
+    required this.px,
+    required this.py,
+  });
+}
